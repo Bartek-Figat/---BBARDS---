9ee8c97df92d5f72998efbe447a0707474230f4d
masterHAWK99 <maciej.pochron99@gmail.com>
2022-08-14 14:30:32 +0200

    ref(FE#7): Move slide list to another component

--- src/components/home/header/swiper/swiper.tsx
+++ src/components/home/header/swiper/swiper.tsx
@@ -6,71 +6,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 import { AdSlide } from "./adSlide";
-import electronics from "assets/images/suggest/electronics.png";
-import hospitality from "assets/images/suggest/hospitality.png";
-import gadgets from "assets/images/suggest/gadgets.png";
-import education from "assets/images/suggest/education.png";
-import software from "assets/images/suggest/software.png";
-import food from "assets/images/suggest/food.png";
-import services from "assets/images/suggest/services.png";
-import animals from "assets/images/suggest/animals.png";
-import automobile from "assets/images/suggest/automobile.png";
-import furniture from "assets/images/suggest/furniture.png";
-import properties from "assets/images/suggest/properties.png";
-import fashion from "assets/images/suggest/fashion.png";
+import { slidesMap } from "./Slides";
 
 export default function SwiperContainer() {
-  let imgTitleMap = [
-    {
-      img: electronics,
-      title: "Electronics",
-    },
-    {
-      img: hospitality,
-      title: "Hospitality",
-    },
-    {
-      img: gadgets,
-      title: "Gadgets",
-    },
-    {
-      img: education,
-      title: "Education",
-    },
-    {
-      img: software,
-      title: "Software",
-    },
-    {
-      img: food,
-      title: "Food",
-    },
-    {
-      img: services,
-      title: "Services",
-    },
-    {
-      img: animals,
-      title: "Animals",
-    },
-    {
-      img: automobile,
-      title: "Automobile",
-    },
-    {
-      img: furniture,
-      title: "Furniture",
-    },
-    {
-      img: properties,
-      title: "Properties",
-    },
-    {
-      img: fashion,
-      title: "fashion",
-    },
-  ];
-
   return (
     <div className="container relative">
       <div className="absolute -bottom-24 w-full">
@@ -88,7 +26,7 @@ export default function SwiperContainer() {
           }}
           autoplay={{ delay: 3000 }}
         >
-          {imgTitleMap.map(({ img, title }) => {
+          {slidesMap.map(({ img, title }) => {
             return (
               <SwiperSlide>
                 <AdSlide img={img} title={title} />
