4235d04c8aa66b61159acd2f550bc8c960c7fe22
Aleksander <aleksader.skorek@gmail.com>
2022-09-04 16:17:42 +0200

    Test commit

--- src/views/home/topCategoriesByAds/LinkSection/LinkSection.tsx
+++ src/views/home/topCategoriesByAds/LinkSection/LinkSection.tsx
@@ -10,8 +10,8 @@ type Props = {
 const LinkSection: React.FC<Props> = ({ to, children }) => {
   return (
     <Link
-      to={to}
       className='mx-auto bg-dark-blue hover:bg-dark-blue-hover ease-in duration-300 text-white px-6 py-4 w-fit rounded-md uppercase font-medium flex justify-center items-center mt-11'
+      to={to}
     >
       <FaEye className='text-xl mr-2' />
       {children}
