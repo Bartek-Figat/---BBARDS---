80191258469119d99134904881c37e70d9443056
masterHAWK99 <maciej.pochron99@gmail.com>
2022-09-14 21:50:00 +0200

    FE#4 Make nav & footer global and implement outlet

--- packages/frontEnd/src/router/PublicRouter.tsx
+++ packages/frontEnd/src/router/PublicRouter.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../views/home";
 import Login from "../views/login/Login";
+import { Layout } from "./Layout";
 
 export function PublicRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
   );
 }
