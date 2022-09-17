21133f9176fab617cc2d493cb098981e681e9904
AntyDeveloper <59744361+AntyDeveloper@users.noreply.github.com>
2022-09-17 23:47:56 +0200

    test

--- packages/backEnd/src/validation/index.ts
+++ packages/backEnd/src/validation/index.ts
@@ -125,7 +125,7 @@ export const userPostAddValidation: RequestHandler = async (req, res, next) => {
     presence: { allowEmpty: false },
     length: {
       minimum: 2,
-      maximum: 300,
+      maximum: 30,
       tooShort: "must be at least %{count} characters",
       tooLong: "it musn't be longer than %{count} characters"
     },
