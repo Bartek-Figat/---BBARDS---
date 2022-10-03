37673d0969d3df0dcf18a91a26617c844550ca27
Adrian <adrian.gorniak97@op.pl>
2022-10-03 20:40:10 +0200

    start creating Ad Post content

--- packages/frontEnd/src/views/admin/addPost/addPost.tsx
+++ packages/frontEnd/src/views/admin/addPost/addPost.tsx
@@ -1,3 +1,11 @@
 export const AddPost = () => {
-  return <div>Hello Add Post</div>;
+  return (
+    <div className="w-100%">
+      <div className="w-full p-5 bg-white rounded-lg text-sm mb-5">
+        <h2 className="font-bold relative mt-2 text-lg border-b pb-2 before:content-[''] before:absolute before:w-1/6 before:h-0.5 before:bg-dark-blue before:-bottom-[1px]">
+          Ad Information
+        </h2>
+      </div>
+    </div>
+  );
 };
