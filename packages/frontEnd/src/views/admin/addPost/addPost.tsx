234e49001238e42fd2a1c9356edaa9d2c8e5ea93
Adrian <adrian.gorniak97@op.pl>
2022-10-09 13:12:05 +0200

    adding a section Author Information

--- packages/frontEnd/src/views/admin/addPost/addPost.tsx
+++ packages/frontEnd/src/views/admin/addPost/addPost.tsx
@@ -64,8 +64,8 @@ export const AddPost = () => {
               placeholder="0"
             />
           </div>
-          <div className="flex justify-between mb-4">
-            <div>
+          <div className="flex justify-between mb-4 tablet:flex-col">
+            <div className="tablet:mb-4">
               <label className="block text-cold-gray text-sm font-bold mb-2">
                 Price Condition
               </label>
@@ -96,7 +96,7 @@ export const AddPost = () => {
                 </li>
               </ul>
             </div>
-            <div>
+            <div className="tablet:mb-4">
               <label className="block text-cold-gray text-sm font-bold mb-2">
                 Ad Category
               </label>
@@ -154,6 +154,57 @@ export const AddPost = () => {
               placeholder="Maximum Of 15 Keywords"
               className="bg-gray-chalk appearance-none w-full h-[20vh] p-4 text-cold-gray leading-tight border-b-2 outline-none focus:border-dark-blue duration-500"
             ></textarea>
+          </div>
+        </form>
+      </div>
+      <div className="w-full p-5 bg-white rounded-lg text-sm mb-5">
+        <h2 className="font-bold relative mt-2 text-lg border-b pb-2 before:content-[''] before:absolute before:w-1/6 before:h-0.5 before:bg-dark-blue before:-bottom-[1px]">
+          Author Information
+        </h2>
+        <form className="my-5">
+          <div className="mb-4">
+            <label className="block text-cold-gray text-sm font-bold mb-2">
+              Name
+            </label>
+            <input
+              className="bg-gray-chalk appearance-none w-full p-4 text-cold-gray leading-tight border-b-2 outline-none focus:border-dark-blue duration-500"
+              id="name"
+              type="text"
+              placeholder="Your Name"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-cold-gray text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              className="bg-gray-chalk appearance-none w-full p-4 text-cold-gray leading-tight border-b-2 outline-none focus:border-dark-blue duration-500"
+              id="email"
+              type="email"
+              placeholder="Your Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-cold-gray text-sm font-bold mb-2">
+              Number
+            </label>
+            <input
+              className="bg-gray-chalk appearance-none w-full p-4 text-cold-gray leading-tight border-b-2 outline-none focus:border-dark-blue duration-500"
+              id="number"
+              type="number"
+              placeholder="Your Number"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-cold-gray text-sm font-bold mb-2">
+              Product Name
+            </label>
+            <input
+              className="bg-gray-chalk appearance-none w-full p-4 text-cold-gray leading-tight border-b-2 outline-none focus:border-dark-blue duration-500"
+              id="adress"
+              type="text"
+              placeholder="Your Adress"
+            />
           </div>
         </form>
       </div>
