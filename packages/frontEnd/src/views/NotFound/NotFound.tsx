91a5679e85fb5684bd5c180b72f6a4d38c4781bc
Aleksander <aleksader.skorek@gmail.com>
2022-10-11 16:52:10 +0200

    FE#42 Add NotFound Component

---
+++ packages/frontEnd/src/views/NotFound/NotFound.tsx
@@ -0,0 +1,22 @@
+import type { FC } from "react";
+import { ButtonLink } from "components/buttons/ButtonLink";
+import { ImHome } from "react-icons/im";
+
+const NotFound: FC = () => {
+  return (
+    <div className="bg-gray-chalk flex flex-col items-center py-20">
+      <h1 className="text-[10rem] md:text-[22rem] mb-6 leading-none text-dark-blue font-bold">
+        404
+      </h1>
+      <p className="text-3xl md:text-5xl mb-6 font-bold">
+        Opps! Something Went Wrong?
+      </p>
+      <ButtonLink to="/" variant="secondary">
+        <ImHome />
+        <span>go to homepage</span>
+      </ButtonLink>
+    </div>
+  );
+};
+
+export default NotFound;
