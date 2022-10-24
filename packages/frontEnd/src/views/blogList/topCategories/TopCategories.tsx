c28dd7117fb7ab5a74c013cfc2cfbdb5eba06799
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2022-10-24 20:27:56 +0200

    FE#43 fix hovers

--- packages/frontEnd/src/views/blogList/topCategories/TopCategories.tsx
+++ packages/frontEnd/src/views/blogList/topCategories/TopCategories.tsx
@@ -6,9 +6,11 @@ interface CategoriesPropCard {
 
 export const TopCategories = ({ key, title, value }: CategoriesPropCard) => {
   return (
-    <div className="flex justify-between items-center border-b ">
-      <div className="text-gray-dark text-lg font-medium">{title}</div>
-      <div className="flex justify-center items-center text-dark-blue bg-white rounded-full my-2 font-medium w-[35px] h-[35px] hover:bg-dark-blue hover:text-white">
+    <div className="flex justify-between items-center border-b group">
+      <div className="text-gray-dark text-lg font-medium group-hover:text-dark-blue group-hover:underline group-hover:underline-offset-1">
+        {title}
+      </div>
+      <div className="flex justify-center items-center text-dark-blue bg-white rounded-full my-2 font-medium w-[35px] h-[35px] group-hover:bg-dark-blue group-hover:text-white">
         <p className="text-sm ">{value}</p>
       </div>
     </div>
